f2d353c0ccd5a8a5d40087e473ba6b53cfc6c245
TamaraBenitez <tamaraelizabethbenitez98@gmail.com>
2021-01-20 21:57:34 -0300

    filtro

--- walletCare/Controllers/UserController.cs
+++ walletCare/Controllers/UserController.cs
@@ -112,9 +112,74 @@ namespace walletCare.Controllers
         }
 
 
+        public IActionResult PorMes() {
+
+            return View();
+        }
+
+
+        public int elegirMes(string mes) {
+
+          
+        var map = new Dictionary<string, int>();
+
+        
+        map.Add("Enero", 1); map.Add("Febrero", 2); map.Add("Marzo", 3);
+        map.Add("Abril", 4);  map.Add("Mayo", 5); map.Add("Junio", 6);
+         map.Add("Julio", 7);  map.Add("Agosto", 8); map.Add("Septiembre", 9);
+          map.Add("Octubre",10);  map.Add("Noviembre", 11);  map.Add("Diciembre", 12);
+
+        
+            return map.FirstOrDefault(m => m.Key == mes).Value;
+
+        }
+
+
+        [HttpPost]
+        public IActionResult filtrarPorMes(string mes) {
+
+             Usuario usuario= HttpContext.Session.Get<Usuario>("UsuarioLogueado");
+            if (usuario != null ) { 
+
+                List<Ingreso> ahorros = new List<Ingreso>();
+                ahorros = db.Ingresos.Where (i => i.fecha.Month.Equals(elegirMes(mes)) && i.mailUsuario.Equals(usuario.Mail)).ToList();
+
+                if(ahorros.Count==0){
+
+                    return View("SinIngresos");
+                }
+
+
+                    else {
+                         return View ("MostrarIngresos",ahorros);
+                   }
+
+           }
+            else {
+
+             return Redirect("/User/ErrorUser");
+            
+            }
+
+              
+        }
+
+
     public IActionResult ErrorUser() {
 
         return View();
+    }
+
+
+    public IActionResult EliminarIngreso(int ID) {
+
+        Ingreso ingreso = db.Ingresos.FirstOrDefault(i => i.ID == ID);
+        
+        db.Ingresos.Remove(ingreso);
+        db.SaveChanges();
+
+        return Redirect("MostrarIngresos");
+
     }
 
 
